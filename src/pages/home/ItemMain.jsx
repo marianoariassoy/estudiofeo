efd2cba32c640c9b80c9862cc65524f867526df9
marianoariassoy <hola@marianoarias.soy>
2023-05-18 12:33:06 -0300

    fixed

--- src/pages/home/ItemMain.jsx
+++ src/pages/home/ItemMain.jsx
@@ -5,9 +5,9 @@ const Item = ({ data, mask }) => {
         <h3 className="text-xl text-center font-bold">{data.title}</h3>
       </div>
 
-      <img src={`/mask/mask-${mask}.svg`} className="item-mask-main absolute z-20 w-full top-0" />
+      <img src={`/assets/mask/mask-${mask}.svg`} className="item-mask-main absolute z-20 w-full top-0" />
 
-      <img src={`/mask/mask-${mask}-outline.svg`} className="item-mask-outline absolute z-10 w-full opacity-0 top-0" />
+      <img src={`/assets/mask/mask-${mask}-outline.svg`} className="item-mask-outline absolute z-10 w-full opacity-0 top-0" />
       <img src={data.image} className={`item-mask-image absolute top-0 h-full w-full object-cover mask mask-${mask} opacity-0`} />
     </a>
   );
